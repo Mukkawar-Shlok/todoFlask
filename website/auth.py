587b810da5c18142122bcda930e2a1e3eba416c1
Shlok Mukkawar <shlokmukkawar2001@gmail.com>
2023-09-19 14:40:44 +0530

    basic database setup

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,15 @@
 # routes of authentication
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    if request.method =='POST':
+        pass
+    data = request.form
+    print(data)
+    return render_template("login.html",text="Testing")
 
 
 @auth.route('/logout')
@@ -13,6 +17,24 @@ def logout():
     return "<h1>logout</h1>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
-    return render_template("sign_up.html")
+    if request.method =='POST':
+        name = request.form.get('userName')
+        email = request.form.get('userEmail')
+        password = request.form.get('userPass')
+        passwordRepeat = request.form.get('userPassConfirm')
+        print(name,email,password,passwordRepeat)
+
+        if len(name)<2:
+            flash("Name should be greater than 2 letter",category="error")
+        elif len(email)<4:
+            flash("Please put valid email",category="error")
+        elif password != passwordRepeat:
+            flash("Password do not match",category="error")
+        elif len(password)<7:
+            flash("Password should be greater than 7 letters",category="error")
+        return render_template("sign_up.html")
+    else:
+        return render_template("sign_up.html")
+        
